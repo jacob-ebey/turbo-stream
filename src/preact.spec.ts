b6c120d4389ff7d4b63e1e431239340fa2c88405
Jacob Ebey <jacob.ebey@live.com>
2025-02-02 00:36:56 -0800

    feat: PSC (Preact Server Components)

---
+++ src/preact.spec.ts
@@ -0,0 +1,449 @@
+import { describe, test, type Mock } from "node:test";
+import { expect } from "expect";
+import {
+	Component,
+	Fragment,
+	h,
+	isValidElement,
+	options,
+	type VNode,
+} from "preact";
+import { Suspense } from "preact/compat";
+import { renderToString, renderToStringAsync } from "preact-render-to-string";
+
+import type { DecodePlugin } from "./decode.js";
+import type { EncodePlugin } from "./encode.js";
+import {
+	decode,
+	encode,
+	type EncodeServerReferenceFunction,
+	type ClientReference,
+	type ServerReference,
+	type DecodeClientReferenceFunction,
+	type EncodeClientReferenceFunction,
+	type DecodeServerReferenceFunction,
+} from "./preact.js";
+
+const CLIENT_REFERENCE = Symbol.for("preact.client.reference");
+const SERVER_REFERENCE = Symbol.for("preact.server.reference");
+
+async function renderWithError(vnode: VNode) {
+	(options as any).errorBoundaries = true;
+	try {
+		return await renderToStringAsync(vnode);
+	} finally {
+		(options as any).errorBoundaries = false;
+	}
+}
+
+function quickDecode<T>(
+	value: T,
+	encodePlugins: EncodePlugin[] = [],
+	decodePlugins: DecodePlugin[] = [],
+	encodeClientReference?: EncodeClientReferenceFunction<any, any>,
+	decodeClientReference?: DecodeClientReferenceFunction<any>,
+	encodeServerReference?: EncodeServerReferenceFunction<any>,
+	decodeServerReference?: DecodeServerReferenceFunction,
+): Promise<T> {
+	const encoded = encode(value, {
+		encodeClientReference,
+		encodeServerReference,
+		plugins: encodePlugins,
+	});
+	return decode(
+		// make sure we can process streams character by character
+		encoded.pipeThrough(
+			new TransformStream({
+				transform(chunk, controller) {
+					for (const char of chunk) {
+						controller.enqueue(char);
+					}
+				},
+			}),
+		),
+		{
+			decodeClientReference,
+			decodeServerReference,
+			plugins: decodePlugins,
+		},
+	);
+}
+
+const compareExcludeKeys = (
+	object1: unknown,
+	object2: unknown,
+	excludeKeys: string[] = [],
+): void => {
+	if (Array.isArray(object1)) {
+		for (let i = 0; i < object1.length; i++) {
+			compareExcludeKeys(object1[i], (object2 as unknown[])[i], excludeKeys);
+		}
+	}
+	if (typeof object1 !== "object" || object1 === null) {
+		expect(object1).toBe(object2);
+	} else {
+		for (const key of Object.keys(object1)) {
+			if (!excludeKeys.includes(key)) {
+				try {
+					expect(object2).toHaveProperty(key);
+				} catch (cause) {
+					throw new Error(`Key ${key} not found in object2`, { cause });
+				}
+				try {
+					compareExcludeKeys(
+						(object1 as any)[key],
+						(object2 as any)[key],
+						excludeKeys,
+					);
+				} catch (cause) {
+					throw new Error(`Key ${key} does not match`, { cause });
+				}
+			}
+		}
+	}
+};
+
+describe("preact", () => {
+	test("can encode and decode basic elements", async () => {
+		const listItems = [
+			h("li", { key: 1 }, "foo"),
+			h("li", { key: 2 }, "bar"),
+			h("li", { key: 3 }, "baz"),
+		];
+		const tree = h("div", { id: "foo" }, h("ul", null, listItems));
+		const decoded = await quickDecode(tree);
+		compareExcludeKeys(decoded, tree, ["__v"]);
+		expect(renderToString(decoded)).toBe(renderToString(tree));
+	});
+
+	test("can encode and decode basic function component", async () => {
+		const rendered = h("div", null, "Hello, world!");
+		function SayHello({ name }: { name: string }) {
+			return rendered;
+		}
+		const tree = h(SayHello, { name: "world" });
+		const decoded = await quickDecode(tree);
+		compareExcludeKeys(decoded, rendered, ["__v"]);
+		expect(renderToString(decoded)).toBe(renderToString(rendered));
+	});
+
+	test("can encode and decode nested function components", async () => {
+		const hello = h("div", null, "Hello, world!");
+		function SayHello({ name }: { name: string }) {
+			return hello;
+		}
+		function Say({ name }: { name: string }) {
+			return [hello, h(SayHello, { name })];
+		}
+		const tree = h(Say, { name: "world" });
+		const decoded = await quickDecode(tree);
+		compareExcludeKeys(decoded, [hello, hello], ["__v"]);
+		expect(renderToString(decoded)).toBe(renderToString(tree));
+	});
+
+	test("can encode and decode async function component", async () => {
+		const rendered = h("div", null, "Hello, world!");
+		async function SayHello({ name }: { name: string }) {
+			return rendered;
+		}
+		const tree = h(SayHello, { name: "world" });
+		const decoded = await quickDecode(tree);
+		expect(isValidElement(decoded)).toBe(true);
+		expect(typeof decoded.type).toBe("function");
+		try {
+			(decoded.type as () => never)();
+			throw new Error("should not reach here");
+		} catch (promise) {
+			expect(promise).toBeInstanceOf(Promise);
+			const lazyMockModule = (await promise) as { default: () => unknown };
+			expect(typeof lazyMockModule.default).toBe("function");
+			const renderedNode = lazyMockModule.default();
+			expect(isValidElement(renderedNode)).toBe(true);
+			compareExcludeKeys(renderedNode, rendered, ["__v"]);
+		}
+		expect(await renderToStringAsync(decoded)).toBe("<div>Hello, world!</div>");
+	});
+
+	test("can encode and decode nested async function components", async () => {
+		const hello = h("div", null, "Hello, world!");
+		async function SayHello({ name }: { name: string }) {
+			return hello;
+		}
+		async function Say({ name }: { name: string }) {
+			return [hello, h(SayHello, { name })];
+		}
+		const tree = h(Say, { name: "world" });
+		const decoded = await quickDecode(tree);
+		expect(isValidElement(decoded)).toBe(true);
+		expect(typeof decoded.type).toBe("function");
+		try {
+			(decoded.type as () => never)();
+			throw new Error("should not reach here");
+		} catch (promise) {
+			expect(promise).toBeInstanceOf(Promise);
+			const lazyMockModule = (await promise) as { default: () => unknown };
+			expect(typeof lazyMockModule.default).toBe("function");
+			const [renderedNode, asyncNode] = lazyMockModule.default() as [
+				preact.VNode,
+				preact.VNode,
+			];
+			expect(isValidElement(renderedNode)).toBe(true);
+			compareExcludeKeys(renderedNode, hello, ["__v"]);
+
+			expect(isValidElement(asyncNode)).toBe(true);
+			expect(typeof asyncNode.type).toBe("function");
+			try {
+				(asyncNode.type as () => never)();
+				throw new Error("should not reach here");
+			} catch (promise) {
+				expect(promise).toBeInstanceOf(Promise);
+				const lazyMockModule = (await promise) as { default: () => unknown };
+				expect(typeof lazyMockModule.default).toBe("function");
+				const renderedNode = lazyMockModule.default();
+				expect(isValidElement(renderedNode)).toBe(true);
+				compareExcludeKeys(renderedNode, hello, ["__v"]);
+			}
+		}
+		expect(await renderToStringAsync(decoded)).toBe(
+			"<div>Hello, world!</div><div>Hello, world!</div>",
+		);
+	});
+
+	test("can encode and decode client reference", async (t) => {
+		const ClientComponent = ({ name }: { name: string }) => {
+			return h("div", null, `Hello, ${name}!`);
+		};
+		ClientComponent.$$typeof = CLIENT_REFERENCE;
+		ClientComponent.$$id = "ClientComponent";
+		const tree = h(ClientComponent, { name: "world" });
+
+		const encodeClientReference = t.mock.fn<
+			EncodeClientReferenceFunction<ClientReference & { $$id: string }, any>
+		>((reference) => {
+			return [reference.$$id];
+		});
+		const decodeClientReference = t.mock.fn<
+			DecodeClientReferenceFunction<[string]>
+		>(async ([id]) => {
+			if (id !== "ClientComponent") {
+				throw new Error("Invalid client reference");
+			}
+			return ClientComponent;
+		});
+
+		const decoded = await quickDecode(
+			tree,
+			undefined,
+			undefined,
+			encodeClientReference,
+			decodeClientReference,
+		);
+		expect(encodeClientReference.mock.callCount()).toBe(1);
+		expect(decodeClientReference.mock.callCount()).toBe(1);
+		expect(isValidElement(decoded)).toBe(true);
+		expect(await renderToStringAsync(h(Fragment, {}, decoded))).toBe(
+			"<div>Hello, world!</div>",
+		);
+	});
+
+	test("can encode and decode multiple client references", async (t) => {
+		const ClientComponent = ({ name }: { name: string }) => {
+			return h("div", null, `Hello, ${name}!`);
+		};
+		ClientComponent.$$typeof = CLIENT_REFERENCE;
+		ClientComponent.$$id = "ClientComponent";
+		const tree = h(Fragment, {}, [
+			h(ClientComponent, { name: "world" }),
+			h(ClientComponent, { name: "world" }),
+		]);
+
+		const encodeClientReference = t.mock.fn<
+			EncodeClientReferenceFunction<ClientReference & { $$id: string }, any>
+		>((reference) => {
+			return [reference.$$id];
+		});
+		const decodeClientReference = t.mock.fn<
+			DecodeClientReferenceFunction<[string]>
+		>(async ([id]) => {
+			if (id !== "ClientComponent") {
+				throw new Error("Invalid client reference");
+			}
+			return ClientComponent;
+		});
+
+		const decoded = await quickDecode(
+			tree,
+			undefined,
+			undefined,
+			encodeClientReference,
+			decodeClientReference,
+		);
+		expect(encodeClientReference.mock.callCount()).toBe(2);
+		expect(decodeClientReference.mock.callCount()).toBe(2);
+		expect(isValidElement(decoded)).toBe(true);
+		expect(await renderToStringAsync(decoded)).toBe(
+			"<div>Hello, world!</div><div>Hello, world!</div>",
+		);
+	});
+
+	test("can encode and decode server reference", async (t) => {
+		const serverFunction = t.mock.fn((name: string) => name) as unknown as Mock<
+			(...args: any[]) => any
+		> &
+			ServerReference & { $$id: string };
+		serverFunction.$$typeof = SERVER_REFERENCE;
+		serverFunction.$$id = "serverFunction";
+
+		const encodeServerReference = t.mock.fn<
+			EncodeServerReferenceFunction<ServerReference & { $$id: string }>
+		>((reference) => {
+			return reference.$$id;
+		});
+
+		const decodeServerReference = t.mock.fn<DecodeServerReferenceFunction>(
+			(encoded) => {
+				if (encoded !== "serverFunction") {
+					throw new Error("Invalid server reference");
+				}
+				return (...args) =>
+					(serverFunction as (...args: any[]) => any).apply(null, args);
+			},
+		);
+
+		const decoded = await quickDecode(
+			serverFunction,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			encodeServerReference,
+			decodeServerReference,
+		);
+		expect(encodeServerReference.mock.callCount()).toBe(1);
+		expect(decodeServerReference.mock.callCount()).toBe(1);
+		expect(typeof decoded).toBe("function");
+		expect(decoded("world")).toBe("world");
+		expect(serverFunction.mock.callCount()).toBe(1);
+	});
+
+	test("can encode and decode server reference as vdom event handler", async (t) => {
+		const serverFunction = t.mock.fn((name: string) => name) as unknown as Mock<
+			(...args: any[]) => any
+		> &
+			ServerReference & { $$id: string };
+		serverFunction.$$typeof = SERVER_REFERENCE;
+		serverFunction.$$id = "serverFunction";
+
+		const encodeServerReference = t.mock.fn<
+			EncodeServerReferenceFunction<ServerReference & { $$id: string }>
+		>((reference) => {
+			return reference.$$id;
+		});
+
+		const decodeServerReference = t.mock.fn<DecodeServerReferenceFunction>(
+			(encoded) => {
+				if (encoded !== "serverFunction") {
+					throw new Error("Invalid server reference");
+				}
+				return (...args) =>
+					(serverFunction as (...args: any[]) => any).apply(null, args);
+			},
+		);
+
+		const button = h("button", { onClick: serverFunction });
+		const decoded = await quickDecode(
+			button,
+			undefined,
+			undefined,
+			undefined,
+			undefined,
+			encodeServerReference,
+			decodeServerReference,
+		);
+		expect(encodeServerReference.mock.callCount()).toBe(1);
+		expect(decodeServerReference.mock.callCount()).toBe(1);
+		compareExcludeKeys(decoded, button, ["__v", "onClick"]);
+		expect(typeof decoded.props.onClick).toBe("function");
+		expect(decoded.props.onClick("world")).toBe("world");
+		expect(serverFunction.mock.callCount()).toBe(1);
+	});
+
+	test("can propagate errors", async () => {
+		function Thrower(): string {
+			throw new Error("fail");
+		}
+
+		class ErrorBoundary extends Component<
+			{ children?: VNode },
+			{ error: Error | null }
+		> {
+			constructor(props: { children?: VNode }) {
+				super(props);
+				this.state = { error: null };
+			}
+			componentDidCatch(error: Error) {
+				this.setState({ error });
+			}
+
+			render() {
+				return this.state.error
+					? h("p", null, this.state.error.message)
+					: this.props.children;
+			}
+			static $$typeof: symbol = CLIENT_REFERENCE;
+		}
+
+		const decoded = await quickDecode(
+			h(ErrorBoundary, null, h(Thrower, null)),
+			undefined,
+			undefined,
+			() => ["ErrorBoundary"],
+			async () => ErrorBoundary,
+		);
+		expect(await renderWithError(h(Fragment, null, decoded))).toBe(
+			"<p>&lt;redacted></p>",
+		);
+	});
+
+	test("can propagate async errors", async () => {
+		async function Thrower() {
+			await new Promise((resolve) => setTimeout(resolve, 0));
+			throw new Error("fail");
+		}
+
+		class ErrorBoundary extends Component<
+			{ children?: VNode },
+			{ error: Error | null }
+		> {
+			constructor(props: { children?: VNode }) {
+				super(props);
+				this.state = { error: null };
+			}
+			componentDidCatch(error: Error) {
+				this.setState({ error });
+			}
+
+			render() {
+				return this.state.error
+					? h("p", null, this.state.error.message)
+					: this.props.children;
+			}
+			static $$typeof: symbol = CLIENT_REFERENCE;
+		}
+
+		const decoded = await quickDecode(
+			h(
+				ErrorBoundary,
+				null,
+				h(Suspense, { fallback: h("p", null, "fallback") }, h(Thrower, null)),
+			),
+			undefined,
+			undefined,
+			() => ["ErrorBoundary"],
+			async () => ErrorBoundary,
+		);
+		expect(renderWithError(h(Fragment, null, decoded))).rejects.toThrowError(
+			"<redacted>",
+		);
+	});
+});
---
+++ src/preact.ts
@@ -0,0 +1,350 @@
+import * as preact from "preact";
+import * as compat from "preact/compat";
+
+import * as turbo from "./turbo-stream.js";
+
+const TYPE_VNODE = "N";
+const TYPE_COMPONENT = "C";
+const TYPE_COMPONENT_ERROR = "E";
+const TYPE_FRAGMENT = "F";
+const TYPE_SUSPENSE = "S";
+const TYPE_CLIENT_REFERENCE = "c";
+const TYPE_SERVER_REFERENCE = "s";
+
+const CLIENT_REFERENCE = Symbol.for("preact.client.reference");
+const SERVER_REFERENCE = Symbol.for("preact.server.reference");
+
+export type ClientReference = {
+	$$typeof: symbol;
+};
+
+export type EncodedClientReference = [...any[]];
+
+export type EncodeClientReferenceFunction<
+	Reference extends ClientReference,
+	Encoded extends EncodedClientReference,
+> = (reference: Reference) => Encoded;
+
+export type ServerReference = {
+	$$typeof: symbol;
+};
+
+export type EncodeServerReferenceFunction<Reference extends ServerReference> = (
+	reference: Reference,
+) => string;
+
+export type DecodeClientReferenceFunction<
+	Encoded extends EncodedClientReference,
+> = (encoded: Encoded) => Promise<preact.ComponentType<any>>;
+
+export type DecodeServerReferenceFunction = (
+	encoded: string,
+) => (...args: any[]) => Promise<unknown>;
+
+export type EncodeOptions = turbo.EncodeOptions & {
+	encodeClientReference?: EncodeClientReferenceFunction<
+		ClientReference,
+		EncodedClientReference
+	>;
+	encodeServerReference?: EncodeServerReferenceFunction<ServerReference>;
+};
+
+export type DecodeOptions = turbo.DecodeOptions & {
+	decodeClientReference?: DecodeClientReferenceFunction<EncodedClientReference>;
+	decodeServerReference?: (
+		encoded: string,
+	) => (...args: unknown[]) => Promise<unknown>;
+};
+
+let preactDecode = ({
+	decodeClientReference,
+	decodeServerReference,
+}: DecodeOptions = {}): turbo.DecodePlugin =>
+	function (pluginType, keyOrRendered, typeOrProps, props) {
+		if (pluginType === TYPE_CLIENT_REFERENCE) {
+			if (!decodeClientReference) {
+				throw new Error("decodeClientReference implementation not provided");
+			}
+			const decodePromise = decodeClientReference(
+				Array.from(arguments).slice(3),
+			);
+
+			return {
+				value: preact.h(
+					compat.lazy(async () => {
+						return {
+							default: await decodePromise,
+						};
+					}),
+					{
+						key: keyOrRendered as string,
+						...(typeOrProps as preact.Attributes),
+					},
+				),
+			};
+		}
+
+		if (pluginType === TYPE_SERVER_REFERENCE) {
+			if (!decodeServerReference) {
+				throw new Error("decodeServerReference implementation not provided");
+			}
+			return {
+				value: decodeServerReference(keyOrRendered as string),
+			};
+		}
+
+		if (pluginType === TYPE_VNODE) {
+			return {
+				value: preact.h(typeOrProps as string, {
+					key: keyOrRendered as string,
+					...(props as preact.Attributes),
+				}),
+			};
+		}
+
+		if (pluginType === TYPE_COMPONENT) {
+			if (
+				typeof keyOrRendered === "object" &&
+				keyOrRendered !== null &&
+				typeof (keyOrRendered as any).then === "function"
+			) {
+				return {
+					value: preact.h(
+						compat.lazy(() =>
+							(keyOrRendered as Promise<unknown>).then((resolved) => ({
+								default: () => resolved as preact.VNode,
+							})),
+						),
+						null,
+					),
+				};
+			}
+			return {
+				value: keyOrRendered,
+			};
+		}
+
+		if (pluginType === TYPE_COMPONENT_ERROR) {
+			return {
+				value: preact.h(() => {
+					throw keyOrRendered;
+				}, {}),
+			};
+		}
+
+		if (pluginType === TYPE_FRAGMENT) {
+			return {
+				value: preact.h(preact.Fragment, {
+					key: keyOrRendered as string,
+					...(typeOrProps as preact.Attributes),
+				}),
+			};
+		}
+
+		if (pluginType === TYPE_SUSPENSE) {
+			return {
+				value: preact.h(compat.Suspense, {
+					key: keyOrRendered as string,
+					...(typeOrProps as preact.ComponentProps<typeof compat.Suspense>),
+				}),
+			};
+		}
+	};
+
+let preactEncode =
+	({
+		encodeClientReference,
+		encodeServerReference,
+	}: EncodeOptions = {}): turbo.EncodePlugin =>
+	(vnode) => {
+		if ((vnode as any).$$typeof === SERVER_REFERENCE) {
+			if (!encodeServerReference) {
+				throw new Error("encodeServerReference implementation not provided");
+			}
+			return [
+				TYPE_SERVER_REFERENCE,
+				encodeServerReference(vnode as ServerReference),
+			];
+		}
+
+		if (preact.isValidElement(vnode)) {
+			// store options hooks once before each synchronous render call
+			let beforeDiff = (preact.options as any)[DIFF];
+			let afterDiff = (preact.options as any)[DIFFED];
+			let renderHook = (preact.options as any)[RENDER];
+			let ummountHook = (preact.options as any).unmount;
+
+			if (beforeDiff) beforeDiff(vnode);
+
+			let { key, props, type } = vnode;
+
+			if (type === preact.Fragment) {
+				if (afterDiff) afterDiff(vnode);
+				if (ummountHook) ummountHook(vnode);
+				return [TYPE_FRAGMENT, key, props];
+			}
+
+			if ((type as any) === compat.Suspense) {
+				return [TYPE_SUSPENSE, key, props];
+			}
+
+			if ((type as any).$$typeof === CLIENT_REFERENCE) {
+				if (!encodeClientReference) {
+					throw new Error("encodeClientReference implementation not provided");
+				}
+				if (afterDiff) afterDiff(vnode);
+				if (ummountHook) ummountHook(vnode);
+				return [
+					TYPE_CLIENT_REFERENCE,
+					key,
+					props,
+					...encodeClientReference(type as unknown as ClientReference),
+				];
+			}
+
+			if (typeof type !== "function") {
+				return [TYPE_VNODE, key, type, props];
+			}
+
+			let isClassComponent =
+				type.prototype && typeof type.prototype.render === "function";
+			if (isClassComponent) {
+				throw new Error("Class components are not supported");
+			}
+
+			const previousSkipEffects = (preact.options as any)[SKIP_EFFECTS];
+			try {
+				(preact.options as any)[SKIP_EFFECTS] = true;
+
+				let component: ComponentType;
+				(vnode as any)[COMPONENT] = component = createComponent(
+					vnode,
+					EMPTY_OBJECT,
+				);
+
+				// If a hook invokes setState() to invalidate the component during rendering,
+				// re-render it up to 25 times to allow "settling" of memoized states.
+				// Note:
+				//   This will need to be updated for Preact 11 to use internal.flags rather than component._dirty:
+				//   https://github.com/preactjs/preact/blob/d4ca6fdb19bc715e49fd144e69f7296b2f4daa40/src/diff/component.js#L35-L44
+				let count = 0;
+				let rendered: unknown;
+				while (component[DIRTY] && count++ < 25) {
+					component[DIRTY] = false;
+
+					if (renderHook) renderHook(vnode);
+
+					try {
+						rendered = (type as preact.FunctionComponent).call(
+							component,
+							props,
+							EMPTY_OBJECT,
+						);
+					} catch (e) {
+						if (asyncMode) {
+							(vnode as any)._suspended = true;
+						}
+						throw e;
+					}
+				}
+				component[DIRTY] = true;
+
+				if (afterDiff) afterDiff(vnode);
+				if (ummountHook) ummountHook(vnode);
+
+				if (preact.options.unmount) preact.options.unmount(vnode);
+
+				return [
+					TYPE_COMPONENT,
+					typeof rendered === "object" &&
+					typeof (rendered as any).then === "function"
+						? (rendered as PromiseLike<unknown>).then(
+								(resolved) => resolved,
+								(error) => {
+									console.log("HERE!!!!!");
+									return preact.h(() => {
+										throw error;
+									}, null);
+								},
+							)
+						: rendered,
+				];
+			} catch (error) {
+				return [TYPE_COMPONENT_ERROR, error];
+			} finally {
+				if ((preact.options as any)[COMMIT])
+					(preact.options as any)[COMMIT](vnode, EMPTY_ARR);
+				(preact.options as any)[SKIP_EFFECTS] = previousSkipEffects;
+				EMPTY_ARR.length = 0;
+			}
+		}
+	};
+
+export const decode = <T>(
+	stream: ReadableStream<string>,
+	options?: DecodeOptions,
+): Promise<T> =>
+	turbo.decode(stream, {
+		...options,
+		plugins: [preactDecode(options), ...(options?.plugins ?? [])],
+	});
+
+export const encode = (value: unknown, options?: EncodeOptions) =>
+	turbo.encode(value, {
+		...options,
+		plugins: [preactEncode(options), ...(options?.plugins ?? [])],
+	});
+
+// Preact internals
+
+const EMPTY_ARR: any[] = [];
+
+// Options hooks
+const DIFF = "__b";
+const RENDER = "__r";
+const DIFFED = "diffed";
+const COMMIT = "__c";
+const CATCH_ERROR = "__e";
+const SKIP_EFFECTS = "__s";
+
+// VNode properties
+const COMPONENT = "__c";
+const CHILDREN = "__k";
+const PARENT = "__";
+const MASK = "__m";
+
+// Component properties
+const VNODE = "__v";
+const DIRTY = "__d";
+const NEXT_STATE = "__s";
+const CHILD_DID_SUSPEND = "__c";
+const EMPTY_OBJECT = {};
+const asyncMode = false;
+
+type ComponentType = {
+	__v: preact.VNode;
+	context: any;
+	props: any;
+	setState: () => void;
+	forceUpdate: () => void;
+	__d: boolean;
+	__h: any[];
+	getChildContext?: () => any;
+};
+function createComponent(vnode: preact.VNode, context: any): ComponentType {
+	return {
+		__v: vnode,
+		context,
+		props: vnode.props,
+		// silently drop state updates
+		setState: markAsDirty,
+		forceUpdate: markAsDirty,
+		__d: true,
+		// hooks
+		__h: new Array(0),
+	};
+}
+
+function markAsDirty(this: ComponentType) {
+	this.__d = true;
+}
